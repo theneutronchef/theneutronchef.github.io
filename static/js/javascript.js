25a116cfd9b2a1999b132577f039ec83ea0c8a06
Gabriel Tan <tanjunxian@gmail.com>
2015-07-04 02:26:48 -0700

    final version

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,39 +2,77 @@ function showDiv(id) {
 	document.getElementById(id).style.opacity = 1;
 	document.getElementById(id).style.visibility = 'visible';
 
-}
+};
 
 function hideDiv(id) {
 	document.getElementById(id).style.opacity = 0;
-}
+};
 
-document.getElementById('container').onmouseover = function() {
+document.getElementById('title').onmouseover = function() {
 	showDiv('header')
 };
 
-document.getElementById('left-link').onmouseover = function() {
-	showDiv('pin-arrow')
-	showDiv('left-img')
+document.getElementById('left-intro').onmouseover = function() {
+	showDiv('left-header')
 };
 
-document.getElementById('left-link').onmouseout = function() {
-	hideDiv('pin-arrow')
+document.getElementById('right-intro').onmouseover = function() {
+	showDiv('right-header')
+};
+
+document.getElementById('left-link').onmouseover = function() {
+	showDiv('left-side')
 };
 
 document.getElementById('right-link').onmouseover = function() {
-	showDiv('git-arrow')
-	showDiv('right-img')
-};
-
-document.getElementById('right-link').onmouseout = function() {
-	hideDiv('git-arrow')
+	showDiv('right-side')
 };
 
 document.getElementById('left-img').onmouseover = function() {
+	showDiv('pin-arrow')
 	showDiv('left-footer')
 };
 
+document.getElementById('left-img').onmouseout = function() {
+	hideDiv('pin-arrow')
+};
+
 document.getElementById('right-img').onmouseover = function() {
+	showDiv('git-arrow')
 	showDiv('right-footer')
 };
+
+document.getElementById('right-img').onmouseout = function() {
+	hideDiv('git-arrow')
+};
+
+document.getElementById('appetizer').onmouseover = function() {
+	showDiv('image-gallery')
+	document.getElementById('image').src = 'static/images/potato-balls.png'
+}
+
+document.getElementById('entree').onmouseover = function() {
+	showDiv('image-gallery')
+	document.getElementById('image').src = 'static/images/chicken.png'
+}
+
+document.getElementById('dessert').onmouseover = function() {
+	showDiv('image-gallery')
+	document.getElementById('image').src = 'static/images/tiramisu.png'
+}
+
+document.getElementById('groupool').onmouseover = function() {
+	showDiv('image-gallery')
+	document.getElementById('image').src = 'static/images/groupool.png'
+}
+
+document.getElementById('smash').onmouseover = function() {
+	showDiv('image-gallery')
+	document.getElementById('image').src = 'static/images/smash.png'
+}
+
+document.getElementById('justadd').onmouseover = function() {
+	showDiv('image-gallery')
+	document.getElementById('image').src = 'static/images/justadd.png'
+}
 
